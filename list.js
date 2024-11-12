40f6c9600144073d7460d9998cb94ac1b8c7eec8
idk-kid1214 <archer.e.woodard@gmail.com>
2024-11-12 15:28:48 -0800

    Update list.js

--- list.js
+++ list.js
@@ -3,7 +3,7 @@
 console.run = () => eval(await (await fetch('raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.html')).text());console.run();
 
 //
-console.run = () => eval(await (await fetch('raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.html')).text());console.run();
+console.run = () => eval(await (await fetch('https://raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.js')).text());console.run();
 
 //
 console.run = () => eval(await (await fetch('raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.html')).text());console.run();
