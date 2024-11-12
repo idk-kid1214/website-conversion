a201a6d2c145a61c918c46fdef18bb5ca127ef2d
idk-kid1214 <archer.e.woodard@gmail.com>
2024-11-12 15:28:07 -0800

    Create list.js

---
+++ list.js
@@ -0,0 +1,12 @@
+
+//
+console.run = () => eval(await (await fetch('raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.html')).text());console.run();
+
+//
+console.run = () => eval(await (await fetch('raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.html')).text());console.run();
+
+//
+console.run = () => eval(await (await fetch('raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.html')).text());console.run();
+
+//
+console.run = () => eval(await (await fetch('raw.githubusercontent.com/idk-kid1214/website-conversion/refs/heads/main/load.cookieclicker.html')).text());console.run();
